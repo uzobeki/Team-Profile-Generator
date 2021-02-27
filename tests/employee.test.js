9b43c3b378f29f19abc64c3bff7dd1e7b4f33c53
Zo Beki <uobeki.40@gmail.com>
2021-02-26 19:01:59 -0500

    test files

---
+++ tests/employee.test.js
@@ -0,0 +1,9 @@
+const { italic } = require('chalk');
+const { clear } = require('console');
+const Employee = require('../lib/Employee');
+
+describe('Employee', () => {
+    it("should creat a new employee instance", () => {
+        const judith = new Employee(judith)
+    });
+});
